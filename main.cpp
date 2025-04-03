19a1a87d0546f3ec68cd261979427aa21c353173
ltb <ltb@DESKTOP-FO8H3TR>
2025-04-04 05:29:41 +0800

    asxd

--- main.cpp
+++ main.cpp
@@ -70,19 +70,18 @@ int main()
 DataFile loadDATA()
 {
 	DataFile DATA;
-	string filepath = "Input/";
-	DATA.LayerPos.load(filepath + "LayerPos.txt");
-	DATA.ku.load(filepath + "ku.txt");
-	DATA.kv.load(filepath + "kv.txt");
-	DATA.theta.load(filepath + "theta.txt");
-	DATA.phi.load(filepath + "phi.txt");
-	DATA.lambda.load(filepath + "lambda.txt");
-	DATA.n_lower.load(filepath + "n_lower.txt");
-	DATA.n_upper.load(filepath + "n_upper.txt");
-	DATA.x.load(filepath + "x.txt");
-	DATA.y.load(filepath + "y.txt");
-	DATA.z.load(filepath + "z.txt");
-	//DATA.layersNum = DATA.LayerPos.size();
+
+	DATA.LayerPos.load("Input/LayerPos.txt");
+	DATA.ku.load("Input/ku.txt");
+	DATA.kv.load("Input/kv.txt");
+	DATA.theta.load("Input/theta.txt");
+	DATA.phi.load("Input/phi.txt");
+	DATA.lambda.load("Input/lambda.txt");
+	DATA.n_lower.load("Input/n_lower.txt");
+	DATA.n_upper.load("Input/n_upper.txt");
+	DATA.x.load("Input/x.txt");
+	DATA.y.load("Input/y.txt");
+	DATA.z.load("Input/z.txt");
 
 	string sreal, simag;
 	mat IndexReal, IndexImag;
@@ -95,8 +94,8 @@ DataFile loadDATA()
 		for (size_t i = 0; i < DATA.LayerPos.size();i++)  // f
 		{
 			DATA.IndexS[j].resize(DATA.LayerPos.size());
-			sreal = filepath + "Index_real_" + "z_" + to_string(i + 1) + "_" + to_string(j+1) + ".txt";
-			simag = filepath + "Index_imag_" + "z_" + to_string(i + 1) + "_" + to_string(j+1) + ".txt";
+			sreal = "Input/Index_real_z_" + to_string(i + 1) + "_" + to_string(j+1) + ".txt";
+			simag = "Input/Index_imag_z_" + to_string(i + 1) + "_" + to_string(j+1) + ".txt";
 
 			IndexReal.load(sreal);
 			IndexImag.load(simag);
